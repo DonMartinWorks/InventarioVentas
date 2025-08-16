d81ba489eba64af62b036bce16c743f0d47f85b3
DonMartinWorks <donmartinworks@gmail.com>
2025-08-16 18:26:34 -0400

    [RAPPASOFT DATATABLE] - Instalación de paquete

---
+++ lang/vendor/wireui/nl_NL/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'empty_options' => 'Lege Opties',
+    'search_here' => 'Zoek hier',
+    'date_picker' => [
+        'months' => ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
+        'days' => ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'],
+        'tomorrow' => 'Morgen',
+        'today' => 'Vandaag',
+        'yesterday' => 'Gisteren',
+    ],
+    'errors' => [
+        'title' => 'Er zaten {errors} fouten in uw inzending',
+    ],
+];
+// TODO: Delete this
