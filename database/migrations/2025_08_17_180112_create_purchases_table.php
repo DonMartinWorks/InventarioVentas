80e6fe973168bf3ba07f2dacb8d60fb0850d6a61
DonMartinWorks <donmartinworks@gmail.com>
2025-08-17 18:41:31 -0400

    [MODELS/MIGRATIONS] - Creación de los archivos

---
+++ database/migrations/2025_08_17_180112_create_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplier_id')->constrained('suppliers')->onDelete('CASCADE');
+            $table->foreignId('purchase_order_id')->constrained('purchase_orders')->onDelete('CASCADE');
+            $table->foreignId('warehouse_id')->constrained('warehouses')->onDelete('CASCADE');
+            $table->integer('voucher_type');
+            $table->string('series');
+            $table->integer('correlative');
+            $table->timestamp('date');
+            $table->decimal('total', 12, 2)->default(0);
+            $table->string('observation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
---
+++ database/migrations/2025_08_17_181601_create_transfers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('origin_warehouse_id')->constrained('warehouses')->onDelete('CASCADE');
+            $table->foreignId('destination_warehouse_id')->constrained('warehouses')->onDelete('CASCADE');
+            $table->integer('voucher_type');
+            $table->string('series');
+            $table->integer('correlative');
+            $table->timestamp('date');
+            $table->decimal('total', 12, 2)->default(0);
+            $table->string('observation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
