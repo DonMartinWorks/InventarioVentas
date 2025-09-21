13676d196ef36666069600be28e953d0768af900
DonMartinWorks <donmartinworks@gmail.com>
2025-09-21 18:58:36 -0300

    [SIDEBAR] - Modificación de los links

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -1,6 +1,9 @@
 @php
     $links = [
-        // Dashboard
+        [
+            'header' => __('Main Content'),
+        ],
+
         [
             'name' => __('Dashboard'),
             'icon' => 'fa-solid fa-grip',
@@ -9,38 +12,141 @@
         ],
 
         [
-            'name' => __('Categories'),
-            'icon' => 'fa-solid fa-tag',
-            'route' => route('admin.categories.index'),
-            'active' => request()->routeIs('admin.categories.*'),
-        ],
-
-        [
-            'name' => __('Products'),
-            'icon' => 'fa-solid fa-box-archive',
-            'route' => route('admin.products.index'),
-            'active' => request()->routeIs('admin.products.*'),
-        ],
-
-        [
-            'name' => __('Customers'),
-            'icon' => 'fa-solid fa-clipboard-user',
-            'route' => route('admin.customers.index'),
-            'active' => request()->routeIs('admin.customers.*'),
-        ],
-
-        [
-            'name' => __('Suppliers'),
-            'icon' => 'fa-solid fa-truck',
-            'route' => route('admin.suppliers.index'),
-            'active' => request()->routeIs('admin.suppliers.*'),
-        ],
-
-        [
-            'name' => __('Warehouses'),
+            'name' => __('Warehouse'),
             'icon' => 'fa-solid fa-warehouse',
-            'route' => route('admin.warehouses.index'),
-            'active' => request()->routeIs('admin.warehouses.*'),
+            'active' => request()->routeIs(['admin.warehouses.*', 'admin.categories.*', 'admin.products.*']),
+            'submenu' => [
+                [
+                    'name' => __('Warehouses'),
+                    'icon' => 'fa-solid fa-warehouse',
+                    'route' => route('admin.warehouses.index'),
+                    'active' => request()->routeIs('admin.warehouses.*'),
+                ],
+
+                [
+                    'name' => __('Categories'),
+                    'icon' => 'fa-solid fa-tag',
+                    'route' => route('admin.categories.index'),
+                    'active' => request()->routeIs('admin.categories.*'),
+                ],
+
+                [
+                    'name' => __('Products'),
+                    'icon' => 'fa-solid fa-box-archive',
+                    'route' => route('admin.products.index'),
+                    'active' => request()->routeIs('admin.products.*'),
+                ],
+            ],
+        ],
+
+        [
+            'name' => __('Purchases'),
+            'icon' => 'fa-solid fa-cart-shopping',
+            'active' => request()->routeIs(['admin.suppliers.*']),
+            'submenu' => [
+                [
+                    'name' => __('Suppliers'),
+                    'icon' => 'fa-solid fa-truck',
+                    'route' => route('admin.suppliers.index'),
+                    'active' => request()->routeIs('admin.suppliers.*'),
+                ],
+
+                [
+                    'name' => __('Purchase Orders'),
+                    'route' => '',
+                    'active' => false,
+                ],
+
+                [
+                    'name' => __('Purchases'),
+                    'route' => '',
+                    'active' => false,
+                ],
+            ],
+        ],
+
+        [
+            'name' => __('Sales'),
+            'icon' => 'fa-solid fa-cash-register',
+            'active' => request()->routeIs('admin.customers.*'),
+            'submenu' => [
+                [
+                    'name' => __('Customers'),
+                    'icon' => 'fa-solid fa-clipboard-user',
+                    'route' => route('admin.customers.index'),
+                    'active' => request()->routeIs('admin.customers.*'),
+                ],
+
+                [
+                    'name' => __('Quotes'),
+                    'route' => '',
+                    'active' => false,
+                ],
+
+                [
+                    'name' => __('Sales'),
+                    'route' => '',
+                    'active' => false,
+                ],
+            ],
+        ],
+
+        [
+            'name' => __('Movements'),
+            'icon' => 'fa-solid fa-arrows-rotate',
+            'active' => false,
+            'submenu' => [
+                [
+                    'name' => __('Inputs and Outputs'),
+                    'route' => '',
+                    'active' => false,
+                ],
+
+                [
+                    'name' => __('Transfers'),
+                    'route' => '',
+                    'active' => false,
+                ],
+            ],
+        ],
+
+        [
+            'name' => __('Reports'),
+            'icon' => 'fa-solid fa-chart-line',
+            'route' => '',
+            'active' => false,
+        ],
+
+        [
+            'header' => __('Configurations'),
+        ],
+
+        [
+            'name' => __('Users'),
+            'icon' => 'fa-solid fa-users',
+            'route' => '',
+            'active' => false,
+        ],
+
+        [
+            'name' => __('Roles'),
+            'icon' => 'fa-solid fa-shield-halved',
+            'route' => '',
+            'active' => false,
+        ],
+
+        [
+            'name' => __('Permissions'),
+            'icon' => 'fa-solid fa-lock',
+            'route' => '',
+            'active' => false,
+        ],
+
+        [
+            'name' => __('Configurations'),
+            'icon' => 'fa-solid fa-gears',
+            'route' => '',
+            'active' => false,
         ],
     ];
 @endphp
