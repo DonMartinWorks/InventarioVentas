f78ee6eb0a6160dcf67402d64b4f9488f5777591
DonMartinWorks <donmartinworks@gmail.com>
2025-10-09 17:12:19 -0300

    [PURCHASE] - Registro de una compra (II)

--- resources/views/livewire/admin/purchase/purchase-create.blade.php
+++ resources/views/livewire/admin/purchase/purchase-create.blade.php
@@ -40,24 +40,26 @@
                 <x-wire-input type="date" label="{{ __('Date') }}" wire:model="date"></x-wire-input>
             </div>
 
-            {{-- Supplier --}}
-            <x-wire-select label="{{ __('Supplier') }}" wire:model="supplier_id" :async-data="[
-                'api' => route('api.suppliers.index'),
-                'method' => 'POST',
-            ]" option-label="name"
-                option-value="id" class="flex-1" required />
-
-            <div class="lg:flex lg:space-x-4">
-                {{-- Product --}}
-                <x-wire-select label="{{ __('Product') }}" wire:model="product_id" :async-data="[
-                    'api' => route('api.products.index'),
+            <div class="grid grid-cols-2 gap-3">
+                {{-- Supplier --}}
+                <x-wire-select label="{{ __('Supplier') }}" wire:model="supplier_id" :async-data="[
+                    'api' => route('api.suppliers.index'),
                     'method' => 'POST',
-                ]" option-label="name"
-                    option-value="id" class="flex-1" required />
+                ]"
+                    option-label="name" option-value="id" class="flex-1" required />
 
-                <div class="flex shrink-0">
-                    <x-wire-button wire:click="addProduct" spinner="addProduct" class="w-full lg:mt-6.5 mt-4" light gray
-                        label="{{ __('Save') }}" right-icon="check" flat interaction:solid="positive" />
+                <div class="lg:flex lg:space-x-4">
+                    {{-- Product --}}
+                    <x-wire-select label="{{ __('Product') }}" wire:model="product_id" :async-data="[
+                        'api' => route('api.products.index'),
+                        'method' => 'POST',
+                    ]"
+                        option-label="name" option-value="id" class="flex-1" required />
+
+                    <div class="flex shrink-0">
+                        <x-wire-button wire:click="addProduct" spinner="addProduct" class="w-full lg:mt-6.5 mt-4" light
+                            gray label="{{ __('Save') }}" right-icon="check" flat interaction:solid="positive" />
+                    </div>
                 </div>
             </div>
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use App\Models\PurchaseOrder;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Database\Eloquent\Builder;
@@ -42,3 +43,44 @@ Route::post('/products', function (Request $request) {
         ->orderBy('name', 'ASC')
         ->get();
 })->name('api.products.index');
+
+Route::get('/purchase-orders', function (Request $request) {
+    $purchaseOrder = PurchaseOrder::when($request->search, function ($query, $search) {
+        $parts = explode('-', $search);
+
+        if (count($parts) == 1) {
+            $query->whereHas('supplier', function ($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                    ->orWhere('document_number', 'like', "%{$search}%");
+            });
+
+            return;
+        }
+
+        if (count($parts) !== 2) {
+            return;
+        }
+
+        $series = $parts[0];
+        $correlative = ltrim($parts[1], '0');
+
+        $query->where('series', $series)
+            ->where('correlative', 'LIKE', "%{$correlative}%");
+    })
+        ->when(
+            $request->exists('selected'),
+            fn(Builder $query) => $query->whereIn('id', $request->input('selected', [])),
+            fn(Builder $query) => $query->limit(10)
+        )
+        ->with(['supplier'])
+        ->orderBy('series', 'ASC')
+        ->get();
+
+    return $purchaseOrder->map(function ($purchaseOrder) {
+        return [
+            'id' => $purchaseOrder->id,
+            'name' => $purchaseOrder->series . '-' . $purchaseOrder->correlative,
+            'description' => $purchaseOrder->supplier->name
+        ];
+    });
+})->name('api.purchase-orders.index');
