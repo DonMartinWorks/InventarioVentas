6149952fd3f5556ba991ca7806ed4e634c76261c
DonMartinWorks <donmartinworks@gmail.com>
2025-10-27 18:31:11 -0300

    [ACTIONS] - Unificación a un componente

--- resources/views/admin/management/sales/actions.blade.php
+++ resources/views/admin/management/sales/actions.blade.php
@@ -1,11 +1,1 @@
-<div class="flex items-center space-x-4">
-    <button
-        class="text-xl transition-all text-neutral-400 hover:bg-orange-600 hover:text-white hover:p-1.5 hover:rounded-md hover:shadow-md hover:text-center">
-        <i class="fa-solid fa-envelope"></i>
-    </button>
-
-    <button
-        class="text-xl transition-all text-neutral-400 hover:bg-teal-600 hover:text-white hover:p-1.5 hover:rounded-md hover:shadow-md hover:text-center">
-        <i class="fa-solid fa-file-pdf"></i>
-    </button>
-</div>
+<x-forms.other-actions />
---
+++ resources/views/components/forms/other-actions.blade.php
@@ -0,0 +1,13 @@
+{{-- TODO: Create links props --}}
+
+<div class="flex items-center space-x-2">
+    <button
+        class="text-xl transition-all bg-white text-neutral-400 hover:bg-yellow-400 hover:text-black border hover:border-black p-2 rounded-md shadow-md">
+        <i class="fa-solid fa-envelope"></i>
+    </button>
+
+    <button
+        class="text-xl transition-all bg-white text-neutral-400 hover:bg-blue-400 hover:text-neutral-200 border hover:border-neutral-200 p-2 rounded-md shadow-md">
+        <i class="fa-solid fa-file-pdf"></i>
+    </button>
+</div>
